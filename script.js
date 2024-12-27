eb4f0e15ba1f5efd9db20aa1babd04645ba60919
johannesmamo <johnmamo827@gmail.com>
2024-12-27 04:23:48 -0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("enter")
+var button = document.getElementById("usrinput")
+
+button.addEventListener("click",function(){
+
+    console.log("click is working")
+})
+
